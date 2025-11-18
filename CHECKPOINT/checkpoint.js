449c80c45527700c4d807eebd1123881ebfe76c3
Dariannys   Daniela Zambrano  Escobar <dariannys.zambrano@globant.com>
2025-11-18 09:05:54 -0500

    update js

--- CHECKPOINT/checkpoint.js
+++ CHECKPOINT/checkpoint.js
@@ -1,9 +1,33 @@
 const form = document.getElementById('articleFormInfo');
 form.addEventListener('submit', function(event) {
- const imagen = document.getElementById('articleImage').value;
+    event.preventDefault();
+ const imagen = document.getElementById('articleImage').src;
     const titulo = document.getElementById('articleTitle').value;
     const descripcion = document.getElementById('articleDescription').value;
-    const fecha= document.getElementById('articleDate').value;
-    const categoria = document.getElementById('articleCategory').value;
+    
 
+    const newform = document.createElement('article');
+    newform.classList.add('Article');
+
+    const img = document.createElement('img');
+    img.classList.add('imagenesArticulos');
+    img.setAttribute('src', imagen);
+
+    const title = document.createElement('h3');
+    title.classList.add('tituloDeLosArticulos');
+    title.innerText = titulo;
+
+    const desc = document.createElement('p');
+    desc.classList.add('ParrafoDeLosArticulos');
+    desc.innerText = descripcion;
+
+    newform.appendChild(img);
+    newform.appendChild(title);
+    newform.appendChild(desc);
+    
+    containerArticlesNews = document.querySelector('.container-articles');
+   // containerArticlesNews.appendChild(newform);
+    
+
+    form.reset();
 });
