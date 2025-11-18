0482fba10cf939ca1b7d5d39d7fc0faa183bbba7
Dariannys   Daniela Zambrano  Escobar <dariannys.zambrano@globant.com>
2025-11-17 21:45:17 -0500

    falta la mitad del js y algunos detalles de la pg web

---
+++ CHECKPOINT/checkpoint.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('articleFormInfo');
+form.addEventListener('submit', function(event) {
+ const imagen = document.getElementById('articleImage').value;
+    const titulo = document.getElementById('articleTitle').value;
+    const descripcion = document.getElementById('articleDescription').value;
+    const fecha= document.getElementById('articleDate').value;
+    const categoria = document.getElementById('articleCategory').value;
+
+});
