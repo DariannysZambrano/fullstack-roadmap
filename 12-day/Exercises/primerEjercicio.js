e627ba0fab157dce357fe6f175b06480207fb25e
Dariannys   Daniela Zambrano  Escobar <dariannys.zambrano@globant.com>
2025-11-21 17:47:33 -0500

    update

---
+++ 12-day/Exercises/primerEjercicio.js
@@ -0,0 +1,6 @@
+//redondee este decimal: 12345.99999 utilizando el metodo floor del objeto new Date();
+
+//resultado
+//let number = 12345.99999;
+//let cosNum = Math.floor(number)
+//console.log(cosNum)
--- app.js
+++ app.js
@@ -5,11 +5,14 @@ let user = {
     ["perrito dary"] : "maria",
 }
 
-let fechaActual = new Date();
+
 
 let mes = fechaActual;
 
 console.log(mes)
+
+
+
 
 let palabra = "Hola Mundo"
 
@@ -17,10 +20,16 @@ let ascii = palabra.charCodeAt(3);
 
 console.log(ascii)
 
+
+
+
 let palabra2 = palabra.replaceAll("o", "i")
 
 console.log(palabra2)
 
+
+
+let fechaActual = new Date();
 let number = -23232322.9999999999999999;
 
 let cosNum = Math.floor(number)
