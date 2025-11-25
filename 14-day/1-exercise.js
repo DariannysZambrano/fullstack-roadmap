17f05fcf01aa6265ee5008a4bca319321b25e7a5
Dariannys   Daniela Zambrano  Escobar <dariannys.zambrano@globant.com>
2025-11-25 15:12:57 -0500

    the second exercise completedgit add .git add .git add .!

--- 14-day/1-exercise.js
+++ 14-day/1-exercise.js
@@ -3,3 +3,21 @@
  * If the two values are the same, then return triple their sum.  
  */
 
+
+function calculatingIntegers() {
+    let integer1 = prompt('ingrese un numero entero');
+    let integer1Change = Number(integer1)
+    let integer2 = prompt('ingrese un segundo numero entero');
+    let integer2Change = Number(integer2)
+
+    let calculatingSum = integer1Change + integer2Change;
+
+    if (integer1Change === integer2Change) {
+       let triple = calculatingSum * 3;
+       console.log(triple);
+    }else{
+    console.log(calculatingSum);
+    }
+}
+
+calculatingIntegers();
