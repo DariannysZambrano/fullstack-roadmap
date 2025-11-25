681c98be2644cde71eca2fe8bf7c69ef680a8331
Dariannys   Daniela Zambrano  Escobar <dariannys.zambrano@globant.com>
2025-11-25 17:02:21 -0500

    update

---
+++ 14-day/6-exercise.js
@@ -0,0 +1,9 @@
+/**
+ * Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.  
+ */
+
+function multiple(){
+     let numeroDado = prompt("Ingrese un número positivo");
+    numeroDado = Number(numeroDado);
+
+}
