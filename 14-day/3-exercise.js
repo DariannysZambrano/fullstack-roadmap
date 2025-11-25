42cc56f8f1f5eeb727a6fa2ee1107e1f0acc8e53
Dariannys   Daniela Zambrano  Escobar <dariannys.zambrano@globant.com>
2025-11-25 17:22:45 -0500

    update ejercicio 3

--- 14-day/3-exercise.js
+++ 14-day/3-exercise.js
@@ -1,4 +1,14 @@
 /**
  * Write a JavaScript program to create another string by adding "Py" in front of a given string
  * If the given string begins with "Py" return the original string.  
- */
+    */
+
+function Py() {
+    let message = prompt("Ingrese un mensaje");
+    let characters = message.split("")
+    console.log(characters);
+    let firstTwoCharacters = message.slice(0, 2);
+    if (firstTwoCharacters === "Py") {
+        return message;
+    }
+}
