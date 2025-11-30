e4316f27ed5d6b60c79d22f824c87c2d3458c3d7
Dariannys   Daniela Zambrano  Escobar <dariannys.zambrano@globant.com>
2025-11-30 11:35:03 -0500

    finish the taks, only one thing to do

---
+++ Ping-Pong/exercise.js
@@ -0,0 +1,37 @@
+// Reglas:
+// - El juego se juega entre 2 jugadores
+// - El jugador 1 comienza sacando
+// - Se cambia de saque cada 2 puntos
+// - Gana el primero que llegue a 11 puntos
+// Objetivo:
+// Crear una función que reciba:
+// - Nombre del jugador 1
+// - Nombre del jugador 2
+// - Puntos actuales del jugador 1
+// - Puntos actuales del jugador 2
+// La función debe retornar:
+// - Quién debe sacar en este momento
+// - Si el juego ha terminado y quién ganó
+
+
+// let namePlayer1 = prompt("Please enter the name of player 1");
+// let namePlayer2 = prompt("Please enter the name of player 2");
+// let pointPlayer1 = Number(prompt("Please enter the point of player 2"));
+// let pointPlayer2 = Number(prompt("Please enter the point of player 2"));
+
+function receiveNamesAndPoints(namePlayer1, namePlayer2,  pointPlayer1, pointPlayer2 ) {
+    let totalPuntos = pointPlayer1 + pointPlayer2
+    if(totalPuntos %2){
+        console.log("Saca el jugador: " + namePlayer2)
+    }else{
+         console.log("Saca el jugador: " + namePlayer1)
+    }
+
+    if(pointPlayer1 === 11){
+        console.log("La ganadora es " + namePlayer1 + " !! 🎉🎊🎀😸")
+    }else if(pointPlayer2 === 11){
+         console.log("La ganadora es " + namePlayer2 + " !! 🎨🎈🧨✨")
+    }
+}
+
+receiveNamesAndPoints("luan", "dary", 10,11);
