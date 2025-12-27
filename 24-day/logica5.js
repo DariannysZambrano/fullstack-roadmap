b0831ad1db0736126385b6c497692e4adb62a94a
DariannysZambrano <daryzaes077@gmail.com>
2025-12-26 21:14:59 -0500

    Add initial medical history JSON file with comprehensive patient data

---
+++ 24-day/logica5.js
@@ -0,0 +1,19 @@
+//  * Crea una funcion que analice texto y obtenga:
+//  * - Número total de palabras.
+//  * - Longitud media de las palabras.
+//  * - Encuentre la palabra más larga.
+
+function analizarTexto(texto) {
+   const resultado = {
+    totalPalabras: 0,
+    promeido: 0;
+    }
+
+  for (let i = 0; i < palabraSplit.length; i++) {
+    palabraLarga ++
+    
+  }
+  return palabraLarga;
+}
+
+console.log(analizarTexto("hola buenos dias compañeros como estan"));
